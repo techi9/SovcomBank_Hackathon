9189bdc9fdedafc86da1718fea00fdd744934c23
brokkko <nsstrannikova@mail.ru>
2022-11-19 21:20:27 +0300

    add webclient connection

--- backend/finance/src/main/java/com/todo/finance/config/CbrConfiguration.java
+++ backend/finance/src/main/java/com/todo/finance/config/CbrConfiguration.java
@@ -1,35 +1,35 @@
-//package com.todo.finance.config;
-//
-//import io.netty.channel.ChannelOption;
-//import io.netty.handler.timeout.ReadTimeoutHandler;
-//import io.netty.handler.timeout.WriteTimeoutHandler;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-//import org.springframework.web.reactive.function.client.WebClient;
-//import reactor.netty.http.client.HttpClient;
-//import reactor.netty.tcp.TcpClient;
-//
-//import java.util.concurrent.TimeUnit;
-//
-//@Configuration
-//public class CbrConfiguration {
-//    private static final String BASE_URL = "http://www.cbr.ru/scripts";
-//    public static final int TIMEOUT = 1000;
-//
-//    @Bean
-//    public WebClient webCbrClientWithTimeout() {
-//        final var tcpClient = TcpClient
-//                .create()
-//                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, TIMEOUT)
-//                .doOnConnected(connection -> {
-//                    connection.addHandlerLast(new ReadTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS));
-//                    connection.addHandlerLast(new WriteTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS));
-//                });
-//
-//        return WebClient.builder()
-//                .baseUrl(BASE_URL)
-//                .clientConnector(new ReactorClientHttpConnector(HttpClient.from(tcpClient)))
-//                .build();
-//    }
-//}
+package com.todo.finance.config;
+
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.tcp.TcpClient;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class CbrConfiguration {
+    private static final String BASE_URL = "http://www.cbr.ru/scripts";
+    public static final int TIMEOUT = 1000;
+
+    @Bean
+    public WebClient webCbrClientWithTimeout() {
+        final var tcpClient = TcpClient
+                .create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, TIMEOUT)
+                .doOnConnected(connection -> {
+                    connection.addHandlerLast(new ReadTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS));
+                    connection.addHandlerLast(new WriteTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS));
+                });
+
+        return WebClient.builder()
+                .baseUrl(BASE_URL)
+                .clientConnector(new ReactorClientHttpConnector(HttpClient.from(tcpClient)))
+                .build();
+    }
+}
